3e5d7f6501ef1fc8fa551918ed4a34137ecb136f
rory foley <rorytfoley@gmail.com>
2025-09-19 10:01:04 -0400

    Redirect / to /games

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
+﻿import { redirect } from "next/navigation";
 export default function Home() {
-  return (
-    <main className="p-6 space-y-4">
-      <h1 className="text-2xl font-semibold">DelphiEdge</h1>
-      <a href="/games" className="inline-block rounded-xl border px-4 py-2 shadow-sm">
-        View NCAAF Games
-      </a>
-    </main>
-  );
+  redirect("/games");
 }
